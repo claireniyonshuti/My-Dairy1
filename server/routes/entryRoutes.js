88767f37f9e16021b5c7248b095ba3aca052f991
claireniyonshuti <claireniyonshuti@akilahinstitute.org>
2019-09-14 08:29:41 +1200

    (ft-modify-entry) Modify an entry of my dairy Finished ft-modify-entry-168484200

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -2,6 +2,7 @@ import express  from 'express';
 import getAllEntries from '../controler/allEntries';
 import getOneEntry from '../controler/getone';
 import postEntry from '../controler/postEntry';
+import updateEntry from '../controler/updateEntry';
 
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.get ('/api/v1/entries', getAllEntries);
 router.get('/api/v1/entries/:id', getOneEntry);
 router.post('/api/v1/entries', postEntry);
+router.put('/api/v1/entries/:id', updateEntry);
 
 export default router;
