9832e29580d5b854cfbbabc1f8edc308ef1ba2f5
claireniyonshuti <claireniyonshuti@akilahinstitute.org>
2019-09-14 08:16:50 +1200

    (ft-add-new-entry) Add new entry of my dairy Finished ft-all-entry-168399417

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -1,11 +1,13 @@
 import express  from 'express';
 import getAllEntries from '../controler/allEntries';
 import getOneEntry from '../controler/getone';
+import postEntry from '../controler/postEntry';
 
 
 const router = express.Router();
 
 router.get ('/api/v1/entries', getAllEntries);
 router.get('/api/v1/entries/:id', getOneEntry);
+router.post('/api/v1/entries', postEntry);
 
 export default router;
